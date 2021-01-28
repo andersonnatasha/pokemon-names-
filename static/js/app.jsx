8bc528929c3ea92a6242ca67176397e2aedde95c
Natasha Anderson <andersonnatashaa@gmail.com>
2021-01-28 22:52:44 +0000

    show loading message

--- static/js/app.jsx
+++ static/js/app.jsx
@@ -6,17 +6,22 @@ function App () {
     const [currentPageUrl, setCurrentPageUrl] = React.useState("https://pokeapi.co/api/v2/pokemon")
     const [nextPageUrl, setNextPageUrl] = React.useState("")
     const [previousPageUrl, setPreviousCurrentPageUrl] = React.useState("")
+    const [loading, setLoading] = React.useState(true)
 
     //When current page changes, rerender code
     React.useEffect(() => {
+        //Show loading message
+        setLoading(true)
         // makes a fetch and returns a promise
         axios.get(currentPageUrl).then(res => {
+            setLoading(false)
             setNextPageUrl(res.data.results.next)
             setPreviousCurrentPageUrl(res.data.results.previous)
             setPokemon(res.data.results.map(p => p.name))
             })
     }, [currentPageUrl])
 
+    if (loading) return "Loading..."
 
 
     return (
