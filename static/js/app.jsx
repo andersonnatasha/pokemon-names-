51b544aa8ffbe33f2e513873eb385274a76cd16d
Natasha Anderson <andersonnatashaa@gmail.com>
2021-01-28 22:49:01 +0000

    set next and previous pages

--- static/js/app.jsx
+++ static/js/app.jsx
@@ -4,11 +4,15 @@
 function App () {
     const [pokemon, setPokemon] = React.useState([])
     const [currentPageUrl, setCurrentPageUrl] = React.useState("https://pokeapi.co/api/v2/pokemon")
+    const [nextPageUrl, setNextPageUrl] = React.useState("")
+    const [previousPageUrl, setPreviousCurrentPageUrl] = React.useState("")
 
     //When current page changes, rerender code
     React.useEffect(() => {
         // makes a fetch and returns a promise
         axios.get(currentPageUrl).then(res => {
+            setNextPageUrl(res.data.results.next)
+            setPreviousCurrentPageUrl(res.data.results.previous)
             setPokemon(res.data.results.map(p => p.name))
             })
     }, [currentPageUrl])
